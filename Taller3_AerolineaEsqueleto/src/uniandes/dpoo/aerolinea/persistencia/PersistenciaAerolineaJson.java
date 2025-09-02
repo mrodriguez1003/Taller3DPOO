343b4cc17add5c4d5bcd4d59461bb41ae6051a97
Mariana Rodriguez <m.rodriguezg234@uniandes.edu.co>
2025-09-02 12:13:23 -0500

    Adelanto

---
+++ Taller3_AerolineaEsqueleto/src/uniandes/dpoo/aerolinea/persistencia/PersistenciaAerolineaJson.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+
+public class PersistenciaAerolineaJson {
+	
+	public void cargarAerolinea( String archivo, Aerolinea aerolinea ) throws IOException, InformacionInconsistenteException
+    {
+        
+    }
+	
+	 public void salvarAerolinea( String archivo, Aerolinea aerolinea ) throws IOException
+	    {
+	        
+	    }
+
+}
